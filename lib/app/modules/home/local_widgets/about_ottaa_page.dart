7525cfbb973a121d2589defd4cb6ddefde9b3106
asimjawad <asimjawad2723@gmail.com>
2022-01-22 19:32:10 +0500

    Navigation Drawer #28 - Added the drawer and the basic about page

---
+++ lib/app/modules/home/local_widgets/about_ottaa_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ottaa_project_flutter/app/modules/home/home_controller.dart';
+
+class AboutOttaaPage extends GetView<HomeController> {
+  const AboutOttaaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
