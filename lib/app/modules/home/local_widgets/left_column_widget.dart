810121fd8eb1c6b05a4df088e03b4bc158576316
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2022-10-05 18:21:18 +0500

    issue#development - removed the app route to games, because games is not done now

--- lib/app/modules/home/local_widgets/left_column_widget.dart
+++ lib/app/modules/home/local_widgets/left_column_widget.dart
@@ -23,20 +23,20 @@ class LeftColumnWidget extends StatelessWidget {
           FittedBox(
             child: GestureDetector(
               onTap: () async{
-                // showDialog(
-                //   context: context,
-                //   barrierDismissible: false,
-                //   barrierColor: Colors.transparent,
-                //   builder: (context) {
-                //     return EmptyTextDialogWidget(
-                //       text: "we_are_working_on_this_feature".tr,
-                //     );
-                //   },
-                // );
-                // await _homeController.startTimerForDialogueExit();
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  barrierColor: Colors.transparent,
+                  builder: (context) {
+                    return EmptyTextDialogWidget(
+                      text: "we_are_working_on_this_feature".tr,
+                    );
+                  },
+                );
+                await _homeController.startTimerForDialogueExit();
                 CustomAnalyticsEvents.setEventWithParameters("Touch",
                     CustomAnalyticsEvents.createMyMap('Principal', 'Games'));
-                Get.toNamed(AppRoutes.GAMES);
+                // Get.toNamed(AppRoutes.GAMES);
               },
               child: Center(
                   child: Icon(
