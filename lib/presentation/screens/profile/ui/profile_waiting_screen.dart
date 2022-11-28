c664968f28a74292f6e9624fc478f0d64b6fe779
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2022-11-28 12:27:40 +0500

    basic UI for waiting screen

--- lib/presentation/screens/profile/ui/profile_waiting_screen.dart
+++ lib/presentation/screens/profile/ui/profile_waiting_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:ottaa_project_flutter/application/common/extensions/translate_string.dart';
 
 class ProfileWaitingScreen extends StatelessWidget {
   const ProfileWaitingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "profile.aguarde".trl,
+            ),
+          ),
+          Text(
+            'Configurando su experiencia...',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
