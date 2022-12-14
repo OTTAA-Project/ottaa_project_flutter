beb1baac8e612085ae4ae0778328d7e1f9f146f0
Héctor Costa <hectoritr@hotmail.com>
2022-12-14 10:15:59 -0300

    Revert "Feat/profile settings"

--- lib/presentation/screens/profile/ui/faq_container_widget.dart
+++ lib/presentation/screens/profile/ui/faq_container_widget.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:ottaa_project_flutter/application/theme/app_theme.dart';
 
 class FaqContainerWidget extends StatelessWidget {
   const FaqContainerWidget({
     Key? key,
     required this.selected,
-    required this.heading,
-    required this.subtitle,
   }) : super(key: key);
   final bool selected;
-  final String heading, subtitle;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +17,6 @@ class FaqContainerWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            width: double.maxFinite,
             padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
             decoration: BoxDecoration(
               color: Colors.white,
@@ -36,48 +31,16 @@ class FaqContainerWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  heading,
+                  "como armo un pictograma?",
                 ),
                 selected
-                    ? const Icon(
+                    ? Icon(
                         Icons.close,
                       )
-                    : const Icon(
+                    : Icon(
                         Icons.add,
                       ),
               ],
-            ),
-          ),
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            height: selected ? 70 : 0,
-            child: SingleChildScrollView(
-              physics: const NeverScrollableScrollPhysics(),
-              child: Column(
-                children: [
-                  const Divider(
-                    height: 3,
-                    color: kOTTAABackground,
-                  ),
-                  Container(
-                    width: double.maxFinite,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 16,
-                      horizontal: 24,
-                    ),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(8),
-                        bottomRight: Radius.circular(8),
-                      ),
-                    ),
-                    child: Text(
-                      subtitle,
-                    ),
-                  ),
-                ],
-              ),
             ),
           ),
         ],
