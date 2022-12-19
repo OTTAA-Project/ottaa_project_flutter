47ccc48889f0c75d12e5f41472303c47e1d92f2b
Emir <emirhassan.ali02@gmail.com>
2022-12-19 13:00:29 -0300

    feat: added a notifier of i18n

--- lib/application/common/i18n.dart
+++ lib/application/common/i18n.dart
@@ -2,9 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ottaa_project_flutter/application/language/translation_tree.dart';
+import 'package:ottaa_project_flutter/application/locator.dart';
 
-class I18N {
+class I18N extends ChangeNotifier {
   final Map<String, TranslationTree> _languages = {};
 
   Locale locale;
@@ -55,15 +58,31 @@ class I18N {
       return null;
     }
   }
-
   TranslationTree? get currentLanguage => _currentLanguage;
 
-  void changeLanguage(String languageCode) {
-    locale = Locale(languageCode.split("_")[0], languageCode.split("_")[1]);
+  Future<void> changeLanguage(String languageCode) async {
+    var split = languageCode.split("_");
+    assert(split.length == 2, "Language code must be in the format: languageCode_countryCode (en_US");
+    locale = Locale(split[0].toLowerCase(), split[1].toUpperCase());
+    TranslationTree? newLanguage = _languages[languageCode] ?? await loadTranslation(locale);
+    if (newLanguage == null) {
+      throw Exception("Language not found");
+    }
+    _languages[languageCode] ??= newLanguage;
     _currentLanguage = _languages[languageCode];
+    notify();
   }
 
-  void changeLanguageFromLocale(Locale locale) {
-    changeLanguage("${locale.languageCode}_${locale.countryCode}");
+  Future<void> changeLanguageFromLocale(Locale locale) async {
+    assert(locale.countryCode != null, "Locale must have a country code");
+    changeLanguage("${locale.languageCode.toLowerCase()}_${locale.countryCode?.toUpperCase()}");
+  }
+
+  void notify() {
+    notifyListeners();
   }
 }
+
+final i18nProvider = ChangeNotifierProvider<I18N>((ref) {
+  return locator<I18N>();
+});
