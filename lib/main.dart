aac0ea10e1a6b5dde3be480c3a8768dffb0ad0ca
Agustin Z <agustinzanoli@gmail.com>
2023-03-20 10:48:22 -0300

    Merged with #209. Looks good, the Prediction fix it is not working at all.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:ottaa_project_flutter/application/injector.dart';
 import 'package:ottaa_project_flutter/application/locator.dart';
 import 'package:ottaa_project_flutter/application/theme/app_theme.dart';
 
-//March 17v1
+//March 20v1
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
