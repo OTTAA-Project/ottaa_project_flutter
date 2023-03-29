2218b3ae8c177eacff2be50aeb8dc4406004df5b
Emir Ali <emirhassan.ali02@gmail.com>
2023-03-29 14:26:52 -0300

    added some margins

--- test/service/LocalStorage/local_storage_service_test.dart
+++ test/service/LocalStorage/local_storage_service_test.dart
@@ -20,111 +20,110 @@ const String kLibraryPath = 'libraryPath';
 const String kApplicationDocumentsPath = 'applicationDocumentsPath';
 const String kExternalCachePath = 'externalCachePath';
 const String kExternalStoragePath = 'externalStoragePath';
-const String kAssetsPath='./';
+const String kAssetsPath = './';
 
-void main(){
+void main() {
   LocalStorageService localStorageService = LocalStorageService();
   setUpAll(() {
     PathProviderPlatform.instance = FakePathProviderPlatform();
     TestWidgetsFlutterBinding.ensureInitialized();
     // expose path_provider
-  });  group('Test Local Storage Service ', () {
-  group('Test es-AR', () {
-    test('Write Groups ES', () async {
-      String result = await rootBundle.loadString('assets/gender_based/grupos/grupos_es_male.json');
-      await localStorageService.writeGruposToFile(data: result, language: 'es_AR');
-    });
-    test('Read Groups ES',() async {
-      List esG = await localStorageService.readGruposFromFile(language:'es_AR');
-      print( esG);
-    });
-    test('WritePictograms ES', () async {
-      String result = await rootBundle.loadString('assets/gender_based/pictos/pictos_es_male.json');
-      await localStorageService.writePictoToFile(data: result, language: 'es_AR');
-    });
-    test('Read Pictograms ES',() async {
-      List esG = await localStorageService.readPictoFromFile(language:'es_AR');
-      print( esG);
-    });
   });
-  group('Test en-US', () {
-    test('Write Groups EN', () async {
-      String result = await rootBundle.loadString('assets/grupos.json');
-      await localStorageService.writeGruposToFile(data: result, language: 'en-US');
+  group('Test Local Storage Service ', () {
+    group('Test es-AR', () {
+      test('Write Groups ES', () async {
+        String result = await rootBundle.loadString('assets/gender_based/grupos/grupos_es_male.json');
+        await localStorageService.writeGruposToFile(data: result, language: 'es_AR');
+      });
+      test('Read Groups ES', () async {
+        List esG = await localStorageService.readGruposFromFile(language: 'es_AR');
+        print(esG);
+      });
+      test('WritePictograms ES', () async {
+        String result = await rootBundle.loadString('assets/gender_based/pictos/pictos_es_male.json');
+        await localStorageService.writePictoToFile(data: result, language: 'es_AR');
+      });
+      test('Read Pictograms ES', () async {
+        List esG = await localStorageService.readPictoFromFile(language: 'es_AR');
+        print(esG);
+      });
     });
-    test('Read Groups EN',() async {
-      List en = await localStorageService.readGruposFromFile(language:'en-US');
-      print( en);
+    group('Test en-US', () {
+      test('Write Groups EN', () async {
+        String result = await rootBundle.loadString('assets/grupos.json');
+        await localStorageService.writeGruposToFile(data: result, language: 'en-US');
+      });
+      test('Read Groups EN', () async {
+        List en = await localStorageService.readGruposFromFile(language: 'en-US');
+        print(en);
+      });
+      test('Read Pictos EN', () async {
+        String result = await rootBundle.loadString('assets/pictos.json');
+        await localStorageService.writePictoToFile(data: result, language: 'en-US');
+      });
+      test('Read Pictos EN', () async {
+        List en = await localStorageService.readPictoFromFile(language: 'en-US');
+        print(en);
+      });
     });
-    test('Read Pictos EN', () async {
-      String result = await rootBundle.loadString('assets/pictos.json');
-      await localStorageService.writePictoToFile(data: result, language: 'en-US');
+    group('Test fr-FR', () {
+      test('Write Groups fr', () async {
+        String result = await rootBundle.loadString('assets/languages/grupos_fr.json');
+        await localStorageService.writeGruposToFile(data: result, language: 'fr-FR');
+      });
+      test('Read Groups fr', () async {
+        List en = await localStorageService.readGruposFromFile(language: 'fr-FR');
+        print(en);
+      });
+      test('Write Pictos fr', () async {
+        String result = await rootBundle.loadString('assets/languages/pictos_fr.json');
+        await localStorageService.writePictoToFile(data: result, language: 'fr-FR');
+      });
+      test('Read Pictos fr', () async {
+        List en = await localStorageService.readPictoFromFile(language: 'fr-FR');
+        print(en);
+      });
     });
-    test('Read Pictos EN',() async {
-      List en = await localStorageService.readPictoFromFile(language:'en-US');
-      print( en);
+    group('Test pt-BR', () {
+      test('Write Groups pt', () async {
+        String result = await rootBundle.loadString('assets/languages/grupos_pt.json');
+        await localStorageService.writeGruposToFile(data: result, language: 'pt-BR');
+      });
+      test('Read Groups pt', () async {
+        List en = await localStorageService.readGruposFromFile(language: 'pt-BR');
+        print(en);
+      });
+      test('Write Pictos pt', () async {
+        String result = await rootBundle.loadString('assets/languages/pictos_pt.json');
+        await localStorageService.writePictoToFile(data: result, language: 'pt-BR');
+      });
+      test('Read Pictos pt', () async {
+        List en = await localStorageService.readPictoFromFile(language: 'pt-BR');
+        print(en);
+      });
     });
-  });
-  group('Test fr-FR', () {
-    test('Write Groups fr', () async {
-      String result = await rootBundle.loadString('assets/languages/grupos_fr.json');
-      await localStorageService.writeGruposToFile(data: result, language: 'fr-FR');
+    group('Test default', () {
+      test('Write Groups IT', () async {
+        String result = await rootBundle.loadString('assets/grupos.json');
+        await localStorageService.writeGruposToFile(data: result, language: 'it-IT');
+      });
+      test('Read Groups IT', () async {
+        List en = await localStorageService.readGruposFromFile(language: 'it-IT');
+        print(en);
+      });
+      test('Read Pictos IT', () async {
+        String result = await rootBundle.loadString('assets/pictos.json');
+        await localStorageService.writePictoToFile(data: result, language: 'it-IT');
+      });
+      test('Read Pictos IT', () async {
+        List en = await localStorageService.readPictoFromFile(language: 'it-IT');
+        print(en);
+      });
     });
-    test('Read Groups fr',() async {
-      List en = await localStorageService.readGruposFromFile(language:'fr-FR');
-      print( en);
-    });
-    test('Write Pictos fr', () async {
-      String result = await rootBundle.loadString('assets/languages/pictos_fr.json');
-      await localStorageService.writePictoToFile(data: result, language: 'fr-FR');
-    });
-    test('Read Pictos fr',() async {
-      List en = await localStorageService.readPictoFromFile(language:'fr-FR');
-      print( en);
-    });
-  });
-  group('Test pt-BR', () {
-    test('Write Groups pt', () async {
-      String result = await rootBundle.loadString('assets/languages/grupos_pt.json');
-      await localStorageService.writeGruposToFile(data: result, language: 'pt-BR');
-    });
-    test('Read Groups pt',() async {
-      List en = await localStorageService.readGruposFromFile(language:'pt-BR');
-      print( en);
-    });
-    test('Write Pictos pt', () async {
-      String result = await rootBundle.loadString('assets/languages/pictos_pt.json');
-      await localStorageService.writePictoToFile(data: result, language: 'pt-BR');
-    });
-    test('Read Pictos pt',() async {
-      List en = await localStorageService.readPictoFromFile(language:'pt-BR');
-      print( en);
-    });
-  });
-  group('Test default', () {
-    test('Write Groups IT', () async {
-      String result = await rootBundle.loadString('assets/grupos.json');
-      await localStorageService.writeGruposToFile(data: result, language: 'it-IT');
-    });
-    test('Read Groups IT',() async {
-      List en = await localStorageService.readGruposFromFile(language:'it-IT');
-      print( en);
-    });
-    test('Read Pictos IT', () async {
-      String result = await rootBundle.loadString('assets/pictos.json');
-      await localStorageService.writePictoToFile(data: result, language: 'it-IT');
-    });
-    test('Read Pictos IT',() async {
-      List en = await localStorageService.readPictoFromFile(language:'it-IT');
-      print( en);
-    });
-  });
-
   });
 }
-class FakePathProviderPlatform extends Fake
-    with MockPlatformInterfaceMixin
-    implements PathProviderPlatform {
+
+class FakePathProviderPlatform extends Fake with MockPlatformInterfaceMixin implements PathProviderPlatform {
   @override
   Future<String?> getTemporaryPath() async {
     return kTemporaryPath;
@@ -168,11 +167,7 @@ class FakePathProviderPlatform extends Fake
   }
 }
 
-
-
-class AllNullFakePathProviderPlatform extends Fake
-    with MockPlatformInterfaceMixin
-    implements PathProviderPlatform {
+class AllNullFakePathProviderPlatform extends Fake with MockPlatformInterfaceMixin implements PathProviderPlatform {
   @override
   Future<String?> getTemporaryPath() async {
     return null;
