05055c88e75b2716c9354ebea9d5fc06b391b075
Agustin Z <agustinzanoli@gmail.com>
2023-03-31 11:26:23 -0300

    speak and delete tested

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:ottaa_project_flutter/application/injector.dart';
 import 'package:ottaa_project_flutter/application/locator.dart';
 import 'package:ottaa_project_flutter/application/theme/app_theme.dart';
 
-//March 31v1
+//March 31v2
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
