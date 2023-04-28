72f70a28eb6768508bc3943fc83cb7598109f2f7
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2023-04-28 12:07:09 +0500

    added remote config files

---
+++ lib/core/repositories/remote_config_repository.dart
@@ -0,0 +1,12 @@
+abstract class RemoteConfigRepository {
+  Future<RemoteConfigRepository> init();
+
+  Future<String?> getString(String key);
+
+  Future<int?> getInt(String key);
+
+  Future<bool?> getBool(String key);
+
+  Future<double?> getDouble(String key);
+
+}
