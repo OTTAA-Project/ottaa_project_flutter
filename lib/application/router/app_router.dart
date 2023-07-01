cf3434fce210533528aa08a4cc59514c5d0edd54
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2023-07-01 21:54:41 +0500

    merging emir changes

--- lib/application/router/app_router.dart
+++ lib/application/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ottaa_project_flutter/application/providers/auth_provider.dart';
 import 'package:ottaa_project_flutter/application/providers/user_provider.dart';
@@ -53,7 +54,7 @@ import 'package:ottaa_project_flutter/presentation/screens/view_board/search_scr
 import 'package:ottaa_project_flutter/presentation/screens/waiting/link_waiting_screen.dart';
 import 'package:ottaa_project_flutter/presentation/screens/waiting/login_waiting_screen.dart';
 
-final appRouterNavigator = StateProvider<GlobalKey<NavigatorState>>((ref) => GlobalKey<NavigatorState>());
+/*final appRouterNavigator = StateProvider<GlobalKey<NavigatorState>>((ref) => GlobalKey<NavigatorState>());
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   final navigatorKey = ref.watch(appRouterNavigator);
@@ -369,4 +370,329 @@ final goRouterProvider = Provider<GoRouter>((ref) {
       ),
     ],
   );
-});
+});*/
+
+class AppRouter {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  LocalDatabaseRepository databaseRepository;
+  AuthRepository authRepository;
+
+  static AppRouter instance = AppRouter._(GetIt.I<LocalDatabaseRepository>(), GetIt.I<AuthRepository>());
+
+  AppRouter._(this.databaseRepository, this.authRepository);
+
+  GoRouter buildRouter() {
+    final user = databaseRepository.user;
+
+    return GoRouter(
+      navigatorKey: navigatorKey,
+      debugLogDiagnostics: kDebugMode,
+      errorBuilder: (context, state) => const ErrorScreen(),
+      initialLocation: "/",
+      restorationScopeId: "root",
+      // refreshListenable: user != null ? databaseRepository.getListeneableFromName(user.type.name) : null,
+      routes: <GoRoute>[
+        GoRoute(
+          path: "/",
+          name: "ottaa",
+          builder: (context, state) => const SplashScreen(),
+        ),
+        GoRoute(
+          path: "/onboarding",
+          name: "onboarding",
+          builder: (context, state) {
+            int? pageIndex = state.extra as int?;
+
+            return OnBoardingScreen(defaultIndex: pageIndex ?? 0);
+          },
+        ),
+        GoRoute(
+          path: "/login",
+          name: "login",
+          builder: (context, state) => const LoginScreen(),
+          routes: [
+            GoRoute(
+              path: "waiting",
+              name: "waiting",
+              builder: (context, state) => const LoginWaitingScreen(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: "/tutorial",
+          name: "tutorial",
+          builder: (context, state) => const TutorialScreen(),
+        ),
+        GoRoute(
+          path: "/home",
+          name: "home",
+          redirect: (_, __) async {
+            bool isLoggedIn = await authRepository.isLoggedIn();
+            final user = await databaseRepository.getUser();
+            if (!isLoggedIn) {
+              return "/login";
+            }
+
+            if (user == null) {
+              return '/';
+            }
+            return null;
+          },
+          builder: (context, state) {
+            final user = databaseRepository.user;
+
+            if (user == null) return Container(); //WAiting for the fetching
+
+            switch (user.type) {
+              case UserType.caregiver:
+                return const ProfileMainScreen();
+              case UserType.user:
+                return const ProfileMainScreenUser();
+              case UserType.none:
+                return const ProfileChooserScreen();
+            }
+          },
+          routes: [
+            GoRoute(
+              path: "loading",
+              name: "loading",
+              builder: (context, state) => const ProfileWaitingScreen(),
+            ),
+            GoRoute(
+              path: "profile",
+              name: "profile",
+              builder: (context, state) => const ProfileSettingsScreen(),
+              routes: [
+                GoRoute(
+                  path: "role",
+                  name: "role",
+                  builder: (context, state) => const ProfileChooserScreenSelected(),
+                ),
+                GoRoute(
+                  path: "accounts",
+                  name: "accounts",
+                  builder: (context, state) => const ProfileLinkedAccountScreen(),
+                ),
+                GoRoute(
+                  path: "tips",
+                  name: "tips",
+                  builder: (context, state) => const ProfileOTTAATipsScreen(),
+                ),
+                GoRoute(
+                  path: "edit",
+                  name: "edit",
+                  builder: (context, state) => const ProfileSettingsEditScreen(),
+                ),
+                GoRoute(
+                  path: "help",
+                  name: "help",
+                  builder: (context, state) => const ProfileHelpScreen(),
+                  routes: [
+                    GoRoute(
+                      path: "faq",
+                      name: "faq",
+                      builder: (context, state) => const ProfileFAQScreen(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            GoRoute(
+              path: "customize",
+              name: "customize",
+              builder: (context, state) => const CustomizedMainTabScreen(),
+              routes: [
+                GoRoute(
+                  path: "board",
+                  name: "board",
+                  builder: (context, state) => const CustomizedBoardTabScreen(),
+                ),
+                GoRoute(
+                  path: "picto",
+                  name: "picto",
+                  builder: (context, state) => const CustomizePictoScreen(),
+                ),
+                GoRoute(
+                  path: "wait",
+                  name: "wait",
+                  builder: (context, state) => const CustomizeWaitScreen(),
+                ),
+              ],
+            ),
+            GoRoute(
+              path: "talk",
+              name: "talk",
+              builder: (context, state) => const HomeScreen(),
+            ),
+
+            GoRoute(
+              path: "account",
+              name: "account",
+              redirect: (_, __) {
+                if (user?.type == UserType.caregiver) {
+                  return null;
+                }
+
+                return "/home";
+              },
+              builder: (context, state) => const SettingScreenUser(),
+              routes: [
+                GoRoute(
+                  path: "layout",
+                  builder: (context, state) => const MainSettingScreen(),
+                ),
+                GoRoute(
+                  path: "accessibility",
+                  builder: (context, state) => const AccessibilityScreen(),
+                ),
+                GoRoute(
+                  path: "tts",
+                  builder: (context, state) => const VoiceAndSubtitleScreen(),
+                ),
+                GoRoute(
+                  path: "language",
+                  builder: (context, state) => const LanguageScreen(),
+                ),
+              ],
+            ),
+            //TODO*: Use ShellRoute instead of GoRoute
+            GoRoute(
+              path: "link",
+              redirect: (_, __) async {
+                final user = await databaseRepository.getUser();
+                if (user?.type == UserType.caregiver) {
+                  return null;
+                }
+
+                return "/home";
+              },
+              builder: (context, state) => const LinkMailScreen(),
+              routes: [
+                GoRoute(
+                  path: "token",
+                  builder: (context, state) => const LinkTokenScreen(),
+                ),
+                GoRoute(
+                  path: "wait",
+                  builder: (context, state) => const LinkWaitingScreen(),
+                ),
+                GoRoute(
+                  path: "success",
+                  builder: (context, state) => const LinkSuccessScreen(),
+                )
+              ],
+            ),
+
+            GoRoute(
+              path: "settings",
+              name: "settings",
+              redirect: (context, state) async {
+                final user = await databaseRepository.getUser();
+
+                if (state.location.startsWith("/home/settings") && user?.type == UserType.caregiver) {
+                  return "/home";
+                }
+
+                return null;
+              },
+              builder: (context, state) => const SettingScreenUser(),
+              routes: [
+                GoRoute(
+                  path: "layout",
+                  builder: (context, state) => const MainSettingScreen(),
+                ),
+                GoRoute(
+                  path: "accessibility",
+                  builder: (context, state) => const AccessibilityScreen(),
+                ),
+                GoRoute(
+                  path: "tts",
+                  builder: (context, state) => const VoiceAndSubtitleScreen(),
+                ),
+                GoRoute(
+                  path: "language",
+                  builder: (context, state) => const LanguageScreen(),
+                ),
+              ],
+            ),
+
+            GoRoute(
+              path: "games",
+              name: "games",
+              builder: (_, __) => const GameScreen(),
+              routes: [
+                GoRoute(
+                  path: 'groups',
+                  builder: (_, __) => const SelectGroupScreen(),
+                  routes: [
+                    GoRoute(
+                      path: 'search',
+                      builder: (_, __) => const SearchScreen(),
+                    ),
+                  ],
+                ),
+                GoRoute(
+                  path: 'match',
+                  builder: (_, __) => const MatchPictogramScreen(),
+                ),
+                GoRoute(
+                  path: 'story',
+                  builder: (_, __) => const ChatGptGame(),
+                  routes: [
+                    GoRoute(
+                      path: 'show',
+                      builder: (_, __) => const ShowCreatedStory(),
+                    ),
+                    GoRoute(
+                      path: 'selectBoard',
+                      builder: (_, __) => const SelectBoardAndPicto(),
+                    ),
+                  ],
+                ),
+                GoRoute(
+                  path: 'memory',
+                  builder: (_, __) => const MemoryGameScreen(),
+                ),
+                GoRoute(
+                  path: 'wtp',
+                  builder: (_, __) => const WhatsThePictoScreen(),
+                ),
+              ],
+            ),
+            GoRoute(
+              path: 'viewBoardsAndPictos',
+              builder: (_, __) => const BoardsPictogramScreen(),
+              routes: [
+                GoRoute(
+                  path: 'search',
+                  builder: (_, __) => const SearchDataScreen(),
+                ),
+                GoRoute(
+                  path: 'editPicto',
+                  builder: (_, __) => const EditPictoScreen(),
+                  routes: [
+                    GoRoute(
+                      path: 'arsaac',
+                      builder: (_, __) => const ChooseArsaacPhotoScreen(),
+                    ),
+                  ],
+                ),
+                GoRoute(
+                  path: 'createPicto',
+                  builder: (_, __) => const CreatePictoPageViewerScreen(),
+                  routes: [
+                    GoRoute(
+                      path: 'arsaac',
+                      builder: (_, __) => const ChooseArsaacPhotoScreen(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
